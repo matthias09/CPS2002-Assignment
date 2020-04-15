48a065faf35c0526f2f6b9e05d19a70b8bff630c
Matthias <matthias.attard.9@gmail.com>
2020-04-15 15:38:19 +0200

    Added generate map class

--- src/main/java/um/cps2002/Map.java
+++ src/main/java/um/cps2002/Map.java
@@ -1,12 +1,45 @@
 package um.cps2002;
+import java.util.Random;
 
 public class Map {
+    private Random rand = new Random();
+
     //Variables
-    int size;
-    String [][] map;
+    public int size;
+    public String [][] map;
 
     Map(int size){
         this.size = size;
         map = new String[size][size];
+    }
+
+    public void generate_Map() {
+        //Since there was no clear number of water tiles I will do size-3 water tiles, 1 treasure the rest green
+
+        //Variables
+        int temp_x, temp_y;
+
+        //Set Treasure
+        map[rand.nextInt(size)][rand.nextInt(size)] = "Y";
+
+        //Set Water
+        for (int a = 0; a < size - 3; a++) {
+            temp_x = rand.nextInt(size);
+            temp_y = rand.nextInt(size);
+            if (map[temp_y][temp_x] == null) {
+                map[temp_y][temp_x] = "B";
+            } else {
+                a--;
+            }
+        }
+
+        //Fill Green Tiles
+        for (int a = 0; a < size; a++) {
+            for (int b = 0; b < size; b++) {
+                if (map[a][b] == null) {
+                    map[a][b] = "G";
+                }
+            }
+        }
     }
 }
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -11,6 +11,4 @@ public class PlayerTest {
       p.position
     }
 */
-
->>>>>>> PlayerRelease
 }
